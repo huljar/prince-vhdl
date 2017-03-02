c452f8484c20e8b7d194d97cef4935883c2c8b3b
Julian Harttung <julian.harttung@web.de>
2017-03-02 17:54:43 +0100

    Implemented PRINCE
    - Added linear layer matrix multiplications
    - Added prince_core module which generates all the rounds
    - The top level module prince_top now only contains the key whitening
      layer and then instantiates prince_core

--- src/prince_top.vhd
+++ src/prince_top.vhd
@@ -8,22 +8,33 @@ entity prince_top is
     );
 end prince_top;
 
-architecture behavioral of prince_top is
+architecture structural of prince_top is
     signal k0_start,
            k0_end,
            k1: std_logic_vector(63 downto 0);
     signal core_in,
            core_out: std_logic_vector(63 downto 0);
 
+    component prince_core
+        port(data_in:  in std_logic_vector(63 downto 0);
+             key:      in std_logic_vector(63 downto 0);
+             data_out: out std_logic_vector(63 downto 0)
+        );
+    end component;
+
     begin
         -- Key extension/whitening keys
-        -- TODO: check if k0_end computation is correct
         k0_start <= key(127 downto 64);
-        k0_end <= ("0" & key(127 downto 65)) xor
-                  (key(126 downto 64) & key(127));
+        k0_end <= key(64) & key(127 downto 66) & (key(65) xor key(127));
         k1 <= key(63 downto 0);
 
-        -- PRINCE_core input/output
+        -- PRINCE_core
         core_in <= plaintext xor k0_start;
         ciphertext <= core_out xor k0_end;
+
+        PC: prince_core port map(
+            data_in => core_in,
+            key => k1,
+            data_out => core_out
+        );
     end architecture;
