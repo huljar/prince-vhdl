53b78b35f171f23c96c02dd039cf890214cbb18f
Paul Walther <paul.walther@tu-dresden.de>
2018-02-16 11:00:08 +0100

    Add decrpytion to core

--- src/prince_top.vhd
+++ src/prince_top.vhd
@@ -3,9 +3,11 @@ use ieee.std_logic_1164.all;
 
 -- The top level module of PRINCE
 entity prince_top is
-    port(plaintext:  in std_logic_vector(63 downto 0);  -- 64 bit plaintext block
-         key:        in std_logic_vector(127 downto 0); -- 128 bit key
-         ciphertext: out std_logic_vector(63 downto 0)  -- 64 bit encrypted block
+    port(intext:  in  std_logic_vector(63  downto 0); -- 64 bit plaintext block
+         key:     in  std_logic_vector(127 downto 0); -- 128 bit key
+         encdec:  in  std_logic;                      -- switch enc/dec
+         
+         outtext: out std_logic_vector(63  downto 0)  -- 64 bit encrypted block
     );
 end prince_top;
 
@@ -28,13 +30,24 @@ architecture structural of prince_top is
 
     begin
         -- Key extension/whitening keys
-        k0_start <= key(127 downto 64);
-        k0_end <= key(64) & key(127 downto 66) & (key(65) xor key(127));
-        k1 <= key(63 downto 0);
+        PK: process (encdec, key) is
+        begin 
+            if encdec = '0' then  -- encryption
+                k0_start <= key(127 downto 64);
+                k0_end <= key(64) & key(127 downto 66) & (key(65) xor key(127));
+                k1 <= key(63 downto 0);
+            else                  -- decryption
+                k0_end <= key(127 downto 64);
+                k0_start <= key(64) & key(127 downto 66) & (key(65) xor key(127));
+                k1 <= key(63 downto 0) xor x"C0AC29B7C97C50DD";
+            end if;
+        end process PK;
+
+        
 
         -- PRINCE_core
-        core_in <= plaintext xor k0_start;
-        ciphertext <= core_out xor k0_end;
+        core_in <= intext xor k0_start;
+        outtext <= core_out xor k0_end;
 
         PC: prince_core port map(
             data_in => core_in,
