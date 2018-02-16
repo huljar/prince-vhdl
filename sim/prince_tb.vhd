1d8b0b3a70f74d7441847449b860698c3759e98c
Paul Walther <paul.walther@tu-dresden.de>
2018-02-16 11:01:16 +0100

    Add decryption to tb

--- sim/prince_tb.vhd
+++ sim/prince_tb.vhd
@@ -43,27 +43,30 @@ ARCHITECTURE behavior OF prince_tb IS
  
     COMPONENT prince_top
     PORT(
-         plaintext : IN  std_logic_vector(63 downto 0);
+         intext : IN  std_logic_vector(63 downto 0);
          key : IN  std_logic_vector(127 downto 0);
-         ciphertext : OUT  std_logic_vector(63 downto 0)
+         encdec: IN std_logic;
+         outtext : OUT  std_logic_vector(63 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal plaintext : std_logic_vector(63 downto 0) := (others => '0');
+   signal intext : std_logic_vector(63 downto 0) := (others => '0');
    signal key : std_logic_vector(127 downto 0) := (others => '0');
+   signal encdec: std_logic := '0';
 
  	--Outputs
-   signal ciphertext : std_logic_vector(63 downto 0);
+   signal outtext : std_logic_vector(63 downto 0);
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: prince_top PORT MAP (
-      plaintext => plaintext,
+      intext => intext,
       key => key,
-      ciphertext => ciphertext
+      encdec => encdec,
+      outtext => outtext
     );
 
    -- Stimulus process
@@ -74,52 +77,105 @@ BEGIN
       wait for 100 ns;
 
       -- Test vectors from the PRINCE paper
+      -- ENCRYPTION
+      encdec <= '0';
       -- First test vector
-      plaintext <= x"0000000000000000";
+      intext <= x"0000000000000000";
       key <= x"00000000000000000000000000000000";
       wait for 1 ns;
-      hwrite(ct, ciphertext);
+      hwrite(ct, outtext);
       report "Ciphertext is " & ct.all & " (expected value: 818665aa0d02dfda)";
       deallocate(ct);
       
       wait for 10 ns;
 
       -- Second test vector
-      plaintext <= x"FFFFFFFFFFFFFFFF";
+      intext <= x"FFFFFFFFFFFFFFFF";
       key <= x"00000000000000000000000000000000";
       wait for 1 ns;
-      hwrite(ct, ciphertext);
+      hwrite(ct, outtext);
       report "Ciphertext is " & ct.all & " (expected value: 604ae6ca03c20ada)";
       deallocate(ct);
       
       wait for 10 ns;
 
       -- Third test vector
-      plaintext <= x"0000000000000000";
+      intext <= x"0000000000000000";
       key <= x"FFFFFFFFFFFFFFFF0000000000000000";
       wait for 1 ns;
-      hwrite(ct, ciphertext);
+      hwrite(ct, outtext);
       report "Ciphertext is " & ct.all & " (expected value: 9fb51935fc3df524)";
       deallocate(ct);
       
       wait for 10 ns;
 
       -- Fourth test vector
-      plaintext <= x"0000000000000000";
+      intext <= x"0000000000000000";
       key <= x"0000000000000000FFFFFFFFFFFFFFFF";
       wait for 1 ns;
-      hwrite(ct, ciphertext);
+      hwrite(ct, outtext);
       report "Ciphertext is " & ct.all & " (expected value: 78a54cbe737bb7ef)";
       deallocate(ct);
       
       wait for 10 ns;
 
       -- Fifth test vector
-      plaintext <= x"0123456789ABCDEF";
+      intext <= x"0123456789ABCDEF";
       key <= x"0000000000000000FEDCBA9876543210";
       wait for 1 ns;
-      hwrite(ct, ciphertext);
+      hwrite(ct, outtext);
       report "Ciphertext is " & ct.all & " (expected value: ae25ad3ca8fa9ccf)";
+      deallocate(ct);
+      
+      wait for 55 ns;
+      -- DECRYPTION
+      encdec <= '1';
+      -- First test vector
+      intext <= x"818665AA0D02DFDA";
+      key <= x"00000000000000000000000000000000";
+      wait for 1 ns;
+      hwrite(ct, outtext);
+      report "Plaintext is " & ct.all & " (expected value: 0000000000000000)";
+      deallocate(ct);
+      
+      wait for 10 ns;
+      
+      -- Second test vector
+      intext <= x"604AE6CA03C20ADA";
+      key <= x"00000000000000000000000000000000";
+      wait for 1 ns;
+      hwrite(ct, outtext);
+      report "Plaintext is " & ct.all & " (expected value: FFFFFFFFFFFFFFFF)";
+      deallocate(ct);
+      
+      wait for 10 ns;
+      
+      -- Third test vector
+      intext <= x"9FB51935FC3DF524";
+      key <= x"FFFFFFFFFFFFFFFF0000000000000000";
+      wait for 1 ns;
+      hwrite(ct, outtext);
+      report "Plaintext is " & ct.all & " (expected value: 0000000000000000)";
+      deallocate(ct);
+      
+      wait for 10 ns;
+      
+      -- Fourth test vector
+      intext <= x"78A54CBE737BB7EF";
+      key <= x"0000000000000000FFFFFFFFFFFFFFFF";
+      wait for 1 ns;
+      hwrite(ct, outtext);
+      report "Plaintext is " & ct.all & " (expected value: 0000000000000000)";
+      deallocate(ct);
+      
+      wait for 10 ns;
+      
+      -- Fifth test vector
+      intext <= x"AE25AD3CA8FA9CCF";
+      key <= x"0000000000000000FEDCBA9876543210";
+      wait for 1 ns;
+      hwrite(ct, outtext);
+      report "Plaintext is " & ct.all & " (expected value: 0123456789ABCDEF)";
       deallocate(ct);
       
       wait;
